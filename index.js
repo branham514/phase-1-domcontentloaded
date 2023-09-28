72468b42a53c1cdf0f495196a0300ac3345c26fe
branham514 <branham.gatheru@student.moringaschool.com>
2023-09-28 21:45:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Target the paragraph element with id "text"
+    const textParagraph = document.getElementById('text');
+  
+    // Change the text content of the paragraph
+    textParagraph.textContent = 'This is really cool!';
+  });
+  
+  
